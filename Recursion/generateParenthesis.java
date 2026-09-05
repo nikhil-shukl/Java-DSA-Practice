cd4b4e2139a39b34e74507a04b3e5d64c107c50f
Nikhil Shukla <nikhilshukla8652@gmail.com>
2026-09-06 01:29:55 +0530

    Add implementation for generating valid parentheses combinations

---
+++ Recursion/generateParenthesis.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class OptimalSolution {
+    public void backtrack(String curr, int open, int close, int n, List<String> res) {
+        if (curr.length() == 2 * n) {
+            res.add(curr);
+            return;
+        }
+        if (open < n) backtrack(curr + "(", open + 1, close, n, res);
+        if (close < open) backtrack(curr + ")", open, close + 1, n, res);
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backtrack("", 0, 0, n, res);
+        return res;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        OptimalSolution sol = new OptimalSolution();
+        List<String> result = sol.generateParenthesis(3);
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
